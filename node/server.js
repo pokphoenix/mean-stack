da05d200c9577e0affdfee34cf21ef13df98c615
komsan.kr <komsan.kr.18@gmail.com>
2019-10-08 16:22:39 +0700

    @add(pok) - add angular - add node - add api - main page call api

---
+++ node/server.js
@@ -0,0 +1,37 @@
+//Backend
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+//Allow client to access cross domain or ip-address
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','content-type,x-access-token');
+    res.setHeader('Access-Control-Allow-Credentials',true);
+    next();
+})
+
+app.get("/",(req,res)=>{
+    res.end("welcome to root path");
+})
+
+app.get("/home",(req,res)=>{
+    res.end("welcome to home");
+})
+
+//dynamic API  return all body data
+app.post("/api",(req,res)=>{
+    const feedback = req.body;
+    console.log(JSON.stringify(feedback));
+    //res.end(JSON.stringify(feedback));
+    feedback.result = "Success";
+    res.json(feedback);
+})
+
+app.listen(3000,()=>{
+    console.log("server is running...");
+})
