b304e09a692ba2ad4e0bfe758ef319d9fa264cff
komsan.kr <komsan.kr.18@gmail.com>
2019-10-10 15:23:49 +0700

    @add(pok) -Observer -Promise -Async Await

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,4 +66,45 @@ export class AppComponent implements OnInit{
     this.com1.change.emit(1000);
     this.com2.reset.emit();
   }
+
+  onObservableLoad(){
+    let callback = result=>{
+      alert(JSON.stringify(result));
+    }
+    this.http.get("https://jsonplaceholder.typicode.com/todos/1").subscribe(callback);
+  }
+
+  onPromiseLoad(){
+    this.http.get("https://jsonplaceholder.typicode.com/todos/1").toPromise().then(result=>{
+      alert(JSON.stringify(result));
+      // return result;
+    }).then(()=>{
+      alert("2")
+    }).then(()=>{
+      alert("3")
+    })
+  }
+
+  onPromiseMultipleLoad(){
+    let p1 = this.http.get("https://jsonplaceholder.typicode.com/todos/1").toPromise();
+    let p2 = this.http.get("https://jsonplaceholder.typicode.com/todos/2").toPromise();
+    let p3 = this.http.get("https://jsonplaceholder.typicode.com/todos/3").toPromise();
+    Promise.all([p1,p2,p3]).then(result=>{
+      alert(JSON.stringify(result[0]));
+      alert(JSON.stringify(result[1]));
+      alert(JSON.stringify(result[2]));
+    })
+  }
+
+  async onAsyncAwaitLoad(){
+    let result1 = await this.http.get("https://jsonplaceholder.typicode.com/todos/1").toPromise()
+    alert(JSON.stringify(result1))
+
+    let result2 = await this.http.get("https://jsonplaceholder.typicode.com/todos/2").toPromise()
+    alert(JSON.stringify(result2))
+
+    let result3 = await this.http.get("https://jsonplaceholder.typicode.com/todos/3").toPromise()
+    alert(JSON.stringify(result3))
+  }
+
 }
